a1adb4ee995308732d1fc83d429bc8fdc0042f5c
santa116-1 <totoplife116@gmail.com>
2024-05-10 00:43:57 +0900

    edit router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./homepage/HomePage";
 import Output from "./output/output";
 import CustomSelect from "./settingpage/SettingPage";
-
-import "./App.css";
 import LogIn from "./authontication/LogIn";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LogIn />} />
+        {/* Redirect from '/' to '/login' */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LogIn />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/output" element={<Output />} />
         <Route path="/setting" element={<CustomSelect />} />
