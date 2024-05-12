41c883fa52dd172b56e1ba67622afc96e50e1a80
santa116-1 <totoplife116@gmail.com>
2024-05-12 20:11:16 +0900

    fix navbar

--- src/component/header.js
+++ src/component/header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="pt-12">
       <nav className="flex justify-between items-center border-b-2 border-gray-200 pb-2 px-20">
         <h1 className="text-2xl mb-5 font-bold">
-          <Link to="/" className="text-2xl mb-5 font-bold">
+          <Link to="/home" className="text-2xl mb-5 font-bold">
             <img alt="Logo" src="images/logo.svg" className="h-10" />
           </Link>
         </h1>
@@ -17,13 +17,13 @@ const Header = () => {
             onClick={() => navigate("/output")}
             className="mr-4 text-blue-500 hover:text-blue-700 hover:underline decoration-blue-700"
           >
-            出力した記事
+            車買取サービス
           </button>
           <button
             onClick={() => navigate("/setting")}
             className="mr-4 text-blue-500 hover:text-blue-700 hover:underline decoration-blue-700"
           >
-            設定
+            作成
           </button>
           <a
             href="https://docs.google.com/spreadsheets/d/1KBuz0tSocys6kA0en05gIjLO9U_ZUMWhVIK8ySZ8nXU/edit#gid=1894772142"
