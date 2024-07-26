a44b8a9e4d91c77faad8da51525d67cbcb3653dd
santa116-1 <totoplife116@gmail.com>
2024-07-27 02:52:24 +0900

    fix progress

--- src/component/sidebar/Progress.js
+++ src/component/sidebar/Progress.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
 // const [credit, setCredit] = useState("");
 // useEffect(() => {
 //     const getCredit = () => {
