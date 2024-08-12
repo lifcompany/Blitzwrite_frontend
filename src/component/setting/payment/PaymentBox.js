f84e5db57f7a6370d5c8c4ec6cd68c977b3bd320
Zihad <zihadmahiuddin@gmail.com>
2024-08-12 23:51:21 +0600

    feat(settings): implement payment method section

--- src/component/setting/payment/PaymentBox.js
+++ src/component/setting/payment/PaymentBox.js
@@ -1,212 +1,263 @@
-import React, { useEffect, useState } from "react";
-import { Button, Container } from "@mui/material";
+import React, { useCallback, useState } from "react";
+import { Backdrop, Button, CircularProgress, Container } from "@mui/material";
 import {
   useStripe,
   useElements,
+  Elements,
   CardNumberElement,
   CardExpiryElement,
   CardCvcElement,
-  Elements,
-  PaymentElement,
 } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-import TestCard from "./TestCard";
+import { loadStripe } from "@stripe/stripe-js";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
 
-
-const stripePromise = loadStripe(
-  "pk_test_51PJzVRFkGHQ8A45rGz0RQ4kWXXo9Ddq82YWhZohEWAvcmLLQvaMykZLGJaHxKgfLOmY6n3X8a2f6mU3dzW7HoAUx00NRu1DK1V",
-  {
-    locale: "ja",
-  }
-);
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY, {
+  locale: "ja",
+});
 
 const ELEMENT_OPTIONS = {
   style: {
     base: {
-      fontSize: '18px',
-      color: '#000',
-      backgroundColor: '#fff',
-      letterSpacing: '0.025em',
-      '::placeholder': {
-        color: '#ddd',
+      fontSize: "18px",
+      color: "#000",
+      backgroundColor: "#fff",
+      letterSpacing: "0.025em",
+      "::placeholder": {
+        color: "#ddd",
       },
     },
     invalid: {
-      color: '#f00',
+      color: "#f00",
     },
   },
 };
 
-const PaymentBox = ({ setOpen }) => {
-  const [clientSecret, setClientSecret] = useState(null);
-  const navigate = useNavigate();
+const PaymentBox = ({ isEdit, onError, onSuccess, setOpen }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <>
+      <Elements
+        stripe={stripePromise}
+        options={{
+          appearance: {
+            labels: "floating",
+          },
+          mode: "setup",
+          currency: "jpy",
+        }}
+      >
+        <Wrapper
+          isEdit={isEdit}
+          onError={onError}
+          onLoadingChange={setIsLoading}
+          onSuccess={onSuccess}
+          setOpen={(isOpen) => {
+            if (!isOpen) {
+              // set loading to false when the modal is closed
+              setIsLoading(false);
+            }
+            setOpen(isOpen);
+          }}
+        />
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={isLoading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </Elements>
+    </>
+  );
+};
 
-  const Wrapper = () => {
-    const stripe = useStripe();
-    const elements = useElements();
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [accountName, setAccountName] = useState(null);
+const Wrapper = ({
+  email,
+  isEdit,
+  onError,
+  onLoadingChange,
+  onSuccess,
+  setOpen,
+}) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [accountName, setAccountName] = useState("");
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  };
 
-      if (!stripe || !elements) {
+  const fetchClientSecret = useCallback(async () => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/setting/setup-intent/`,
+        {},
+        { headers }
+      );
+      return response.data.clientSecret;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }, [email]);
+
+  const updatePaymentMethod = useCallback(async (paymentMethodId) => {
+    try {
+      const response = await axios.patch(
+        `${process.env.REACT_APP_API_URL}/api/setting/payment-method/?id=${paymentMethodId}`,
+        {},
+        { headers }
+      );
+      return response.status >= 200 && response.status < 300;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }, []);
+
+  if (!stripe || !elements) {
+    return null;
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    onLoadingChange(true);
+    try {
+      await elements.submit();
+
+      const clientSecret = await fetchClientSecret();
+
+      const result = await stripe.confirmCardSetup(clientSecret, {
+        payment_method: { card: elements.getElement("cardNumber") },
+      });
+
+      if (result.error) {
+        console.log(result);
+        if (result.error.message) {
+          onError(result.error.message);
+        }
+        setOpen(false);
         return;
       }
 
-      const result = await stripe.confirmPayment({
-        elements,
-        confirmParams: {
-          return_url: `${process.env.PUBLIC_URL}/confirm-payment`,
-        },
-      });
-      if (result.error) {
-        console.log(result);
-        setErrorMessage(result.error.message);
+      if (!result.setupIntent.payment_method) {
+        onError("支払い情報の登録に失敗しました");
+        setOpen(false);
+        return;
+      }
+
+      const paymentMethodUpdated = await updatePaymentMethod(
+        typeof result.setupIntent.payment_method === "string"
+          ? result.setupIntent.payment_method
+          : result.setupIntent.payment_method.id
+      );
+      if (paymentMethodUpdated) {
+        onSuccess("支払い情報を登録しました");
+        setOpen(false);
       } else {
-        navigate("/success");
+        onError("支払い情報の登録に失敗しました");
+        setOpen(false);
       }
-    };
-
-    useEffect(() => {
-      if (errorMessage) {
-        alert(errorMessage);
-      }
-    }, [errorMessage]);
-
-    return (
-      <Container maxWidth="sm" style={{ marginTop: "10px" }}>
-        <h2 className="heading font text-[calc(2vmin)] font-semibold mt-10 mb-10">
-          クレジットカードの登録
-        </h2>
-        <form onSubmit={handleSubmit}>
-          {/* <PaymentElement /> */}
-          <div className="pt-14">
-            <label className="block font-bold ml-3 mb-2 font-sans text-black" htmlFor="number">
-              カード番号
-            </label>
-            <div className="input-wrapper bg-white p-[10px] border hover:border border-zinc-400 rounded credit-bottom-shadow hover:border-orange-500">
-              <CreditCardIcon />
-              <CardNumberElement
-                id="number"
-                options={ELEMENT_OPTIONS}
-              />
-            </div>
-          </div>
-
-          <div className="pt-8">
-            <label className="block font-bold ml-3 mb-2 text-black" htmlFor="name">
-              カード名義
-            </label>
-            <div className="bg-white p-[10px] border hover:border border-zinc-400 rounded credit-bottom-shadow hover:border-orange-500">
-              <input
-                type="text"
-                value={accountName ?? ''}
-                onChange={(event) => {
-                  setAccountName(event.target.value ?? '');
-                }}
-                id='name'
-                placeholder='山田 太郎'
-                className="outline-none"
-              />
-            </div>
-          </div>
-          <div className=" flex items-center justify-between gap-10">
-            <div className="pt-8 w-1/2">
-              <label className="block font-bold mb-2 text-black" htmlFor="expiry">
-                有効期限
-              </label>
-              <div className="bg-white p-[10px] border hover:border border-zinc-400 rounded credit-bottom-shadow hover:border-orange-500">
-                <CardExpiryElement
-                  id="expiry"
-                  options={ELEMENT_OPTIONS}
-                />
-              </div>
-            </div>
-            <div className="pt-8 w-1/2">
-              <label className="block font-bold mb-2 text-black" htmlFor="cvc">
-                セキュリティコード
-              </label>
-              <div className="bg-white p-[10px] border hover:border border-zinc-400 rounded credit-bottom-shadow hover:border-orange-500">
-                <CardCvcElement
-                  id="cvc"
-                  options={{ ...ELEMENT_OPTIONS, placeholder: '1234' }}
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="pt-4 pb-28">
-            <p>カード裏面に記載されている番号の右３桁または</p>
-            <p>４桁の数字を入力してください</p>
-          </div>
-
-          <div className="flex justify-end items-center gap-5">
-            <div className="flex items-center justify-between">
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                style={{ marginTop: "10px" }}
-                className="w-full py-2"
-              >
-                編集
-              </Button>
-            </div>
-            <button
-              className="text-blue-500 roundedtransition mt-[10px]"
-              onClick={() => setOpen(false)}
-            >
-              キャンセル
-            </button>
-          </div>
-        </form>
-      </Container>
-    );
-  };
-
-  useEffect(() => {
-    if (localStorage.getItem("accessToken") == null) {
-      navigate("/login");
+    } catch (error) {
+      console.error(error);
+      onError("支払い情報の登録に失敗しました");
+      setOpen(false);
     }
-    const fetchClientSecret = async () => {
-      const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
-      };
-      console.log(localStorage.getItem("accessToken"));
-      try {
-        const response = await axios.post(
-          `${process.env.REACT_APP_API_URL}/api/setting/create-payment-intent/`,
-          { email: "santabaner1223@gmail.com" },
-          { headers },
-        );
-        console.log(response);
-        setClientSecret(response.data.clientSecret);
-      } catch (error) {
-        navigate("/login");
-      }
-    };
-
-    fetchClientSecret();
-  }, [navigate]);
-
-  const options = {
-    clientSecret,
-    appearance: {},
+    onLoadingChange(false);
   };
 
   return (
-    <>
-      {clientSecret && (
-        <Elements stripe={stripePromise} options={options}>
-          <Wrapper />
-        </Elements>
-      )}
-    </>
+    <Container maxWidth="sm" className="my-4">
+      <h2 className="heading font text-[calc(2vmin)] font-semibold mt-6 mb-6">
+        クレジットカードの登録
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="">
+          <label
+            className="block font-bold ml-3 mb-2 font-sans text-black"
+            htmlFor="number"
+          >
+            カード番号
+          </label>
+          <div className="flex items-center gap-2 bg-white p-[10px] border hover:border border-zinc-400 rounded credit-bottom-shadow hover:border-orange-500">
+            <CreditCardIcon />
+            <CardNumberElement
+              className="flex-1 text-center"
+              id="number"
+              options={ELEMENT_OPTIONS}
+            />
+          </div>
+        </div>
+
+        <div className="pt-8">
+          <label
+            className="block font-bold ml-3 mb-2 text-black"
+            htmlFor="name"
+          >
+            カード名義
+          </label>
+          <div className="bg-white p-[10px] border hover:border border-zinc-400 rounded credit-bottom-shadow hover:border-orange-500">
+            <input
+              type="text"
+              value={accountName}
+              onChange={(event) => {
+                setAccountName(event.target.value ?? "");
+              }}
+              id="name"
+              placeholder="山田 太郎"
+              className="outline-none autofill"
+            />
+          </div>
+        </div>
+        <div className=" flex items-center justify-between gap-10">
+          <div className="pt-8 w-1/2">
+            <label className="block font-bold mb-2 text-black" htmlFor="expiry">
+              有効期限
+            </label>
+            <div className="bg-white p-[10px] border hover:border border-zinc-400 rounded credit-bottom-shadow hover:border-orange-500">
+              <CardExpiryElement id="expiry" options={ELEMENT_OPTIONS} />
+            </div>
+          </div>
+          <div className="pt-8 w-1/2">
+            <label className="block font-bold mb-2 text-black" htmlFor="cvc">
+              セキュリティコード
+            </label>
+            <div className="bg-white p-[10px] border hover:border border-zinc-400 rounded credit-bottom-shadow hover:border-orange-500">
+              <CardCvcElement
+                id="cvc"
+                options={{ ...ELEMENT_OPTIONS, placeholder: "123" }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-end items-center gap-5">
+          <div className="flex items-center justify-between">
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              style={{ marginTop: "10px", backgroundColor: "#0F1740" }}
+              className="w-full py-2"
+            >
+              {isEdit ? "編集" : "追加"}
+            </Button>
+          </div>
+          <button
+            className="text-blue-500 roundedtransition mt-[10px]"
+            onClick={() => setOpen(false)}
+          >
+            キャンセル
+          </button>
+        </div>
+      </form>
+    </Container>
   );
 };
 
--- src/page/setting/SettingPayment.js
+++ src/page/setting/SettingPayment.js
@@ -1,56 +1,75 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import Header from "../../component/common/header";
 import Notification from "../../component/common/notification";
 import Error from "../../component/common/error";
 import SettingMenu from "../../component/common/SettingMenu";
-import PaymentCard from "../payment-card";
-import { FormControl, OutlinedInput } from "@mui/material";
-import PaymentModal from "../../component/setting/payment/PaymentModal";
-import { Button } from '@mui/material';
-import CreditCardForm from "../../component/setting/payment/CreditCardForm";
+import { Backdrop, Box, CircularProgress, Modal } from "@mui/material";
+import PaymentBox from "../../component/setting/payment/PaymentBox";
+import PaymentMethodCard from "../../component/setting/payment/PaymentMethodCard";
+import { Button } from "@mui/material";
+
+const fetchPaymentMethod = async () => {
+  const res = await axios.get(
+    `${process.env.REACT_APP_API_URL}/api/setting/payment-method/`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }
+  );
+  if (res.status >= 200 && res.status < 300) {
+    return res.data;
+  } else {
+    throw new Error("Failed to fetch payment method");
+  }
+};
 
 const SettingPayment = () => {
+  const [paymentBoxOpen, setPaymentBoxOpen] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const [error, setError] = useState("");
-  const [notification, setNotification] = useState("");
-  const [cardInfo, setCardInfo] = useState(null);
-  const [showPaymentCard, setShowPaymentCard] = useState(false);
+  const refetchPaymentMethod = useCallback(async () => {
+    setIsLoading(true);
+    fetchPaymentMethod()
+      .then((paymentMethod) => {
+        setPaymentMethod(paymentMethod);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [setPaymentMethod]);
 
-
-  const [open, setOpen] = useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-
-
-  useEffect(() => {
-    const apiUrl = process.env.REACT_APP_API_URL;
-    const fetchCardInfo = async () => {
-      try {
-        const response = await axios.get(`${apiUrl}/api/setting/get-user-card-info/`, {
+  const deletePaymentMethod = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      await axios.delete(
+        `${process.env.REACT_APP_API_URL}/api/setting/payment-method/`,
+        {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
           },
-        });
-        setCardInfo(response.data);
-      } catch (error) {
-        console.error("Error fetching card info:", error);
-        setError("カード情報を取得できませんでした。");
-      }
-    };
-    fetchCardInfo();
+        }
+      );
+      await refetchPaymentMethod();
+      setSuccessMessage("支払い情報を削除しました");
+    } catch (err) {
+      console.error(err);
+      setErrorMessage("支払い情報の削除に失敗しました");
+    }
+    setIsLoading(false);
   }, []);
 
-  const registerPayment = () => {
-    setShowPaymentCard((prevShowPaymentCard) => !prevShowPaymentCard); // Toggle PaymentCard visibility
-  };
+  useEffect(() => {
+    refetchPaymentMethod();
+  }, [refetchPaymentMethod]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
@@ -62,46 +81,73 @@ const SettingPayment = () => {
           <h1 className="heading font text-[calc(8px+2vmin)] text-gray-900 font-semibold mt-16">
             支払い
           </h1>
-          <h2 className=" heading font text-[calc(2vmin)] font-semibold mt-10 mb-10">
+          <h2 className="heading font text-[calc(2vmin)] font-semibold mt-8 mb-4">
             クレジットカード情報
           </h2>
-          <div className="mt-5">
-            <form noValidate autoComplete="off">
-              <FormControl sx={{ width: "25ch" }}>
-                <OutlinedInput
-                  value={
-                    cardInfo
-                      ? `${cardInfo.card_brand} ending in ${cardInfo.card_last4} (Exp: ${cardInfo.card_exp_month}/${cardInfo.card_exp_year})`
-                      : "支払い情報がありません"
-                  }
-                  disabled
-                />
-              </FormControl>
-            </form>
-          </div>
-          <div className=" py-4">
-            <PaymentModal />
-          </div>
-          <div className=" py-4">
-            <button
-              onClick={() => registerPayment()}
-              className="bg-blue-700 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-800 focus:outline-none"
-            >
-              支払い情報を登録
-            </button>
-          </div>
-          {showPaymentCard && <PaymentCard />}
-        </div>
-
-        <div className="App">
-          <Button variant="outlined" onClick={handleClickOpen}>
-            クレジットカードの登録
+          {paymentMethod ? (
+            <PaymentMethodCard
+              paymentMethod={paymentMethod}
+              deletePaymentMethod={deletePaymentMethod}
+            />
+          ) : (
+            <>
+              <p className="mx-2 mt-4 mb-8">支払い情報がありません</p>
+            </>
+          )}
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ backgroundColor: "#0F1740" }}
+            className="py-2"
+            onClick={() => setPaymentBoxOpen(true)}
+          >
+            {paymentMethod ? "支払い情報を更新" : "支払い情報を登録"}
           </Button>
-          <CreditCardForm open={open} handleClose={handleClose} />
+          <Modal
+            keepMounted
+            open={paymentBoxOpen}
+            onClose={() => setPaymentBoxOpen(false)}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              sx={{
+                bgcolor: "background.paper",
+                boxShadow: 24,
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: "30%",
+                borderRadius: "5px",
+              }}
+            >
+              <PaymentBox
+                isEdit={!!paymentMethod}
+                onSuccess={(successMessage) => {
+                  // refetchPaymentMethod();
+                  setSuccessMessage(successMessage);
+                }}
+                onError={(errorMessage) => {
+                  setErrorMessage(errorMessage);
+                }}
+                setOpen={setPaymentBoxOpen}
+              />
+            </Box>
+          </Modal>
         </div>
       </div>
-      <Notification content={notification} />
-      <Error content={error} />
+      <Notification content={successMessage} />
+      <Error content={errorMessage} />
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isLoading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </div>
   );
 };
