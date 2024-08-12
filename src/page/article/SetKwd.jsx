9990148da9fab023b681721993743195887ce82d
santa116-1 <totoplife116@gmail.com>
2024-08-13 04:16:52 +0900

    fix disable

--- src/page/article/SetKwd.jsx
+++ src/page/article/SetKwd.jsx
@@ -17,9 +17,10 @@ const SetKwd = () => {
   const [options, setOption] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
   const [error, setError] = useState("");
-  const [selectionError, setSelectionError] = useState("");
+
 
   const navigate = useNavigate();
+
   const apiUrl = process.env.REACT_APP_API_URL;
 
   const handleKeyDown = (event) => {
@@ -32,7 +33,9 @@ const SetKwd = () => {
             .post(`${apiUrl}/api/generate/keyword-suggest/`, { keyword: keyword })
             .then((response) => {
               console.log(response.data);
-              setSuggestions(response.data.suggestions);
+
+              setSuggestions(response.data.suggestions)
+
             })
             .catch((error) => {
               console.error("Backend Error:", error);
@@ -48,14 +51,6 @@ const SetKwd = () => {
   };
 
   const handleResultClick = (result) => {
-    const selectedSuggestion = suggestions.find((s) => s.keyword === result);
-
-    if (selectedSuggestion && selectedSuggestion.avg_monthly_searches < 10) {
-      setSelectionError("このキーワードの検索ボリュームが不十分です。");
-      return;
-    }
-
-    setSelectionError("");
     setSelectedResults((prevSelectedResults) => {
       if (prevSelectedResults.includes(result)) {
         return prevSelectedResults.filter((r) => r !== result);
@@ -64,8 +59,7 @@ const SetKwd = () => {
       }
     });
   };
-
-  const runProcess = () => {
+  const runProcess = (result) => {
     if (selectedResults.length > 2) {
       navigate("/artgen/progress", { state: { selectedResults } });
     } else {
@@ -89,7 +83,7 @@ const SetKwd = () => {
             },
           }
         );
-        setOption(response.data[1]);
+        setOption(response.data[1])
       } catch (error) {
         console.error("Error fetching suggestions:", error);
         setSuggestions([]);
@@ -184,8 +178,8 @@ const SetKwd = () => {
                   className={`p-2 pl-3 pr-3 border rounded-2xl mr-3 mb-2 ${
                     selectedResults.includes(suggestion.keyword)
                       ? "bg-[#232E2F] text-white"
-                      : suggestion.avg_monthly_searches < 10
-                      ? "bg-red-500 text-white" // Error style for low search volume
+                      : suggestion.avg_monthly_searches < 100
+                      ? " bg-gray-100 text-[#232E2F]" // Error style for low search volume
                       : "bg-white text-[#232E2F] border-[#001021] border-[1px]"
                   } hover:bg-[#232E2F] hover:text-white`}
                   onClick={() => handleResultClick(suggestion.keyword)}
@@ -194,7 +188,6 @@ const SetKwd = () => {
                 </button>
               ))}
             </div>
-            {selectionError && <p className="text-red-500 mt-2">{selectionError}</p>}
           </div>
         </div>
         <div className="fixed bottom-0 bg-white w-screen transition-all duration-900 bg-opacity-60  backdrop-blur-md ">
