6086609a7e2650e8e42fc2ad3337d1d5c1a29ac9
santa116-1 <totoplife116@gmail.com>
2024-08-15 15:29:00 +0900

    fix header

--- src/component/common/IconGroup.js
+++ src/component/common/IconGroup.js
@@ -1,28 +1,22 @@
-import React from "react";
-import Badge from '@mui/material/Badge';
+import React, { useEffect, useState } from "react";
 import UserMenu from "./userMenu";
 import SettingIcon from "../topbar/SettingIcon";
 import NotificationIcon from "../topbar/NotificationIcon";
 import HistoryIcon from "../topbar/HistoryIcon";
-
 const IconGroup = () => {
+  const [arrayNoti, setArrayNoti] = useState([]);
+
+  useEffect(() => {
+    setArrayNoti(["記事作成完了 13:00", "記事作成完了 15:05"]);
+  }, []); 
+
   return (
     <div className="flex items-end gap-7 rounded-full">
       <nav className="block max-w-full bg-transparent text-white shadow-none transition-all px-0 py-1 ">
         <div className="flex flex-col-reverse md:flex-row md:items-center justify-end gap-6">
           <div className="flex items-center justify-end gap-7">
             <SettingIcon />
-            <Badge badgeContent={4} color="error"
-              sx={{
-                '& .MuiBadge-standard': {
-                  width: 17, 
-                  height: 17, 
-                  fontSize: '0.7rem', 
-                },
-              }}
-              >
-              <NotificationIcon />
-            </Badge>
+            <NotificationIcon count_noti={2} array_noti={arrayNoti} />
             <HistoryIcon />
           </div>
         </div>
